d75e167ac3a23605a3cc865a5286eb31f1c68d9b
Harim Tanko <harimtanko@gmail.com>
2022-06-26 00:58:08 +0100

    User sign in route

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['12344'] }));
+
+app.get('/signup', (req, res) => {
   res.send(`
+  <div>
+    Your id is: ${req.session.userId}
     <form method="POST">
         <input name='email' placeholder="email" />
         <input name="password" placeholder="password" />
         <input name="passwordConfirmation" placeholder="confirm password" />
         <button>Sign up</button>
     </form>
-
+    </div>
   `);
 });
 
-app.post('/', (req, res) => {
-  res.send('Account creacted insha allah');
+// const bodyParser = (req, res, next) => {
+//   if (req.method === 'POST') {
+//     req.on('data', (data) => {
+//       const parsed = data.toString('utf8').split('&');
+
+//       let formData = {};
+
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split('=');
+
+//         formData[key] = value;
+//       }
+//       req.body = formData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// };
+
+app.post('/signup', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.findOneBy({ email });
+
+  if (existingUser) {
+    return res.send('Email is already in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords do not match salam');
+  }
+
+  //Create a user
+  const user = await usersRepo.create({ email, password });
+
+  //Store id of the user inside the users cookie
+  req.session.userId = user.id;
+
+  res.send('Account created masha allah');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
 });
 
 app.listen(3000, () => {
---
+++ repositories/users.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const { create } = require('domain');
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a new repository requires a filename');
+    }
+
+    this.filename = filename;
+
+    try {
+      fs.accessSync(this.filename);
+    } catch (error) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+
+  async getAll() {
+    //open  the file called this.filename
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
+  }
+
+  async create(attr) {
+    attr.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attr);
+
+    this.writeAll(records);
+
+    return attr;
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+
+    return records.find((records) => records.id === id);
+  }
+
+  async deleteOne(id) {
+    const records = await this.getAll();
+
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attr) {
+    const records = await this.getAll();
+
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`record with id of ${id} not found`);
+    }
+
+    Object.assign(record, attr);
+
+    await this.writeAll(records);
+  }
+
+  async findOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) found = false;
+      }
+
+      if (found) {
+        return record;
+      }
+    }
+  }
+}
+
+module.exports = new UsersRepository('users.json');
